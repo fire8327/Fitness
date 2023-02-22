691f521e74d03ed0a2b4b52509c6cd35226799f1
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-02-22 10:53:39 +0000

    create main

---
+++ Assets/JS/custom.js
@@ -0,0 +1,19 @@
+/* navigation */
+/*jshint esnext: true */
+const toggler = document.getElementById("toggler");
+const mNav = document.getElementById("mobile__nav");
+const close = document.getElementById("toggler__expanded");
+const overlay = document.getElementById("overlay");
+const body = document.getElementsByTagName("body")[0];
+
+toggler.addEventListener("click", () => {
+  mNav.classList.remove("-translate-y-full");
+  overlay.classList.remove("hidden");
+  body.classList.add("overflow-hidden");
+});
+
+close.addEventListener("click", () => {
+  mNav.classList.add("-translate-y-full");
+  overlay.classList.add("hidden");
+  body.classList.remove("overflow-hidden"); 
+});
