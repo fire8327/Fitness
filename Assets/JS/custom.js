51ec79a5abf59f45960df81beaff679afbf405a0
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-02-23 13:46:11 +0000

    create tables

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -17,3 +17,37 @@ close.addEventListener("click", () => {
   overlay.classList.add("hidden");
   body.classList.remove("overflow-hidden"); 
 });
+
+/* tables */
+let cell, i, j0, j1, j2;
+document.querySelector('#table1').onmouseover = (event) => {
+  cell = event.target;
+  if (cell.tagName.toLowerCase() != 'td')
+    return;
+  i = cell.parentNode.children[0];
+  j0 = cell.cellIndex;  
+  j1 = document.querySelector("#head1");
+  j2 = j1.children[j0];
+  j2.classList.add("text-[#F7931E]");
+  i.classList.add("text-[#F7931E]");
+}
+document.querySelector('#table1').onmouseout = (event) => {
+  j2.classList.remove("text-[#F7931E]");
+  i.classList.remove("text-[#F7931E]");
+}
+let cell2, i2, j02, j12, j22;
+document.querySelector('#table2').onmouseover = (event) => {
+  cell2 = event.target;
+  if (cell2.tagName.toLowerCase() != 'td')
+    return;
+  i2 = cell2.parentNode.children[0];
+  j02 = cell2.cellIndex;  
+  j12 = document.querySelector("#head2");
+  j22 = j12.children[j02];
+  j22.classList.add("text-[#F7931E]");
+  i2.classList.add("text-[#F7931E]");
+}
+document.querySelector('#table2').onmouseout = (event) => {
+  j22.classList.remove("text-[#F7931E]");
+  i2.classList.remove("text-[#F7931E]");
+}
